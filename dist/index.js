0fb9d25971ee6543cb3b642425188e3b49a5cdf8
VJS <minsung_96@naver.com>
2021-04-01 23:57:01 +0900

    typescript 2

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHello = (name, age, gender) => {
+    return `Hello ${name}, you are ${age} years old, and you are a ${gender}`;
+};
+sayHello("VJS", 25, "Male");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHello = (name: string, age: number, gender: string): string =>{
+    return `Hello ${name}, you are ${age} years old, and you are a ${gender}`
+}
+
+sayHello("VJS", 25, "Male")
+
+export {}
